7abda5d806ce4b3489045356d68b73f727c067a0
lie5571 <LiE5571@sduhsd.lan>
2016-09-08 08:52:11 -0700

    added onto lesson 2

---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,11 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 5;
+		int recArea = l * w;
+		
+		System.out.println("The area of your rectangle is" +" "+recArea);
+	}
+}
