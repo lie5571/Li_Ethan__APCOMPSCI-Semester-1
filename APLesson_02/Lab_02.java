57e42f5ae944d56c88cfc4233142ab3070a08ba5
lie5571 <LiE5571@sduhsd.lan>
2016-09-08 08:54:51 -0700

    more lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,31 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int q = 3;
+		int p = 7;
+		int e = 2;
+		int r = 6;
+		int l = 1;
+		int h = 5;
+		int w = 8;
+		int mult1 = q * p;
+		int mult2 = e * r;
+		int surArea = 2 * l * w + 2 * l * h + 2 * h * w;
+		String first = "Ethan Li";
+		String second = "3887 Pell Place";
+		String third = "San Diego";
+		String fourth = "92130";
+		
+		System.out.println(q + " " +"multiplied by" + " " + p + " " + "is" + " " + mult1);
+		System.out.println(e + " " +"multiplied by" + " " + r + " " + "is" + " " + mult2);
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+		System.out.println(fourth);
+		System.out.println("The surface area of your rectangle is" + " " + surArea);
+		
+	}
+	
+	
+}
