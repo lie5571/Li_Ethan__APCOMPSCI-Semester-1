8564e520d0d83a990b255f9f4ac6284727654cb0
lie5571 <Yifanli2009@gmail.com>
2016-11-01 09:10:31 -0700

    ex 06

--- APLesson_05/Ex_06.java
+++ APLesson_05/Ex_06.java
@@ -2,16 +2,33 @@ import java.util.Scanner;
 
 public class Ex_06
 {
+	static String Password, Username;
 	public static void main(String[]args)
+	{	
+		Username = "BOB";
+		Password = "Joe";
+		
+		passCheck();
+		
+	}
+	
+	public static void passCheck()
 	{
 		Scanner kb = new Scanner(System.in);
-		String Username  BOB
-		String Password = Joe
 		
 		System.out.println("Please enter the Username: ");
 		String user1 = kb.next();
-		System.out,println("Please enter the Password: ");
+		System.out.println("Please enter the Password: ");
 		String pass1 = kb.next();
 		
+		if(user1.equals(Username) && pass1.equals(Password))
+			System.out.println("You are granted access!");
+		else
+			if(user1.equals(Username) && ! pass1.equals(Password))
+				System.out.println("Your password is incorrect");
+			else if(! user1.equals(Username) && pass1.equals(Password))
+				System.out.println("Your username is incorrect");
+			else
+				System.out.println("Your username and password are incorrect");
 	}
 }
